7b348c88e47d228552c6bb2b94db8696e301fa06
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2025-05-15 15:37:24 -0700

    Create variables.tf

---
+++ terraform-modules/terraform-aws-s3-bucket/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "versioning_enabled" {
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket"
+  type        = map(string)
+  default     = {}
+}
